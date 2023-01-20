abbef17a5dbb842f989d1ccd0b68f5200bf8d719
RiskiTrinaldo <114551002+RiskiTrinaldo@users.noreply.github.com>
2023-01-20 18:38:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<!-- Reprogramed By Riski Trinaldo -->
+<?php
+	$conn = new mysqli("localhost","root","","cart_system");
+	if($conn->connect_error){
+		die("Connection Failed!".$conn->connect_error);
+	}
+?>
